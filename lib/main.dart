57824484e504b8e16b3e047c17eda32d8861ae89
Wishman <gjwisher@aol.com>
2020-01-09 14:37:00 +0100

    READ version: provide data in top node (MyApp) and read in Level3 w/o passing through Level 1 & 2

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart'; // 2.1
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  final String data = 'my passed through data'; // 2.2
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Container(),
+    // 3.1:
+    return Provider<String>(
+      create: (context) => data, // use create instead of builder as per 4.0!
+      child: MaterialApp(
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text(data), // 2.3
+          ),
+          body: Level1(),
         ),
-        body: Level1(),
       ),
     );
   }
@@ -37,6 +43,6 @@ class Level2 extends StatelessWidget {
 class Level3 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Text('Needs Data');
+    return Text(Provider.of<String>(context)); // 3.2
   }
 }
