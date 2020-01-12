03421848e0954cc9dd19f8aac62981ee860c83c9
Wishman <gjwisher@aol.com>
2020-01-12 12:41:17 +0100

    NOT WORKINg due to issues with provider 4.0 package

--- lib/main.dart
+++ lib/main.dart
@@ -4,16 +4,21 @@ import 'package:provider/provider.dart'; // 2.1
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  final String data = 'my passed through data'; // 2.2
+  //final String data = 'my passed through data'; // 2.2 obsolete as per 4.3 (see below)
   @override
   Widget build(BuildContext context) {
+    /* obsolete with 4.4
     // 3.1:
     return Provider<String>(
       create: (context) => data, // use create instead of builder as per 4.0!
+    */
+    // 4.4:
+    return ChangeNotifierProvider<Data>(
+      create: (context) => Data(), //4.4
       child: MaterialApp(
         home: Scaffold(
           appBar: AppBar(
-            title: Text(data), // 2.3
+            title: MyText(), // 2.3 (obsolete) & 4.1(b)
           ),
           body: Level1(),
         ),
@@ -34,8 +39,12 @@ class Level1 extends StatelessWidget {
 class Level2 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Level3(),
+    // 4.2(b)
+    return Column(
+      children: [
+        MyTextField(),
+        Level3(),
+      ],
     );
   }
 }
@@ -43,6 +52,39 @@ class Level2 extends StatelessWidget {
 class Level3 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Text(Provider.of<String>(context)); // 3.2
+    // return Text(Provider.of<String>(context)); // 3.2 obsolete with 4.5
+    return Text(Provider.of<Data>(context).data); // 4.5
+  }
+}
+
+// 4.1(a)
+class MyText extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    //return Text('myText needs data'); //obsolete with 4.6
+    return Text(Provider.of<Data>(context).data); // 4.6
+  }
+}
+
+// 4.2(a)
+class MyTextField extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      onChanged: (newText) {
+        Provider.of<Data>(context).changeString(newText); // 4.8 !!!
+      },
+    );
+  }
+}
+
+// 4.3
+class Data with ChangeNotifier {
+  String data = 'some dummy data in Data class';
+
+  // 4.7:
+  void changeString(String newString) {
+    data = newString;
+    notifyListeners();
   }
 }
